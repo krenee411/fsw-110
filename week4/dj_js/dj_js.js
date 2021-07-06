532008d9eedff647c1a5131cfb8e995c972601e7
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-07-06 17:30:03 -0500

    updated dj_js

--- week4/dj_js/dj_js.js
+++ week4/dj_js/dj_js.js
@@ -1,48 +1,66 @@
 var div = document.createElement("div")
-div.setAttribute("class", "box")
+div.setAttribute("id", "box")
 document.body.style.width= "300px"
 document.body.style.height= "300px"
 document.body.style.border= "black 2px solid"
-document.body.style.backgroundColor= "white"
 document.body.appendChild(div)
 
-var button= document.createElement("button")
-button.textContent= "click here"
-document.body.appendChild(button)
+var div1 = document.createElement("div")
+for (var i=0; i<= 20; i++){
+    var div1 = document.createElement("h1")
+div1.textContent= "Hello World"
+document.body.appendChild(div1)
+document.body.style.textAlign= "right"
+}
 
-const btn=document.querySelector("button")
+
 
 function one(){
-    document.body.style.backgroundColor= "black"
+    div.body.style.backgroundColor= "black"
 
 }
 
 function two(){
     document.body.style.backgroundColor= "green"
 
-}
+}  
 
 function three(){
     document.body.style.backgroundColor= "yellow"
 }
 
 function four(){
-    document.body.style.backgroundColor= "blue"
+    document.body.style.backgroundColor= "red"
 }
 
 function five(){
-    document.body.style.backgroundColor= "red"
+    document.body.style.backgroundColor= "blue"
 }
 
 function six(){
     document.body.style.backgroundColor= "purple"
 }
 
-btn.addEventListener("load", one);
-btn.addEventListener("mouseover", two );
-btn.addEventListener("keydown", three );
-btn.addEventListener("dblclick", four );
-btn.addEventListener("keyup", five );
-btn.addEventListener("scroll", six );
+window.addEventListener("load", one);
+window.addEventListener("mouseover", two);
+window.addEventListener("mousedown", three);
+window.addEventListener("dblclick", four);
+window.addEventListener("mouseup", five);
+window.addEventListener("scroll", six);
 
-document.div.appendChild(btn)
+window.onkeydown = function(e) {
+var key = e.keyCode ? e.keyCode : e.which;
+
+   if (key == 87) {
+    document.body.style.backgroundColor= "white";
+   }else if (key == 66) {
+    document.body.style.backgroundColor= "blue";
+   }else if (key == 71) {
+    document.body.style.backgroundColor= "green";
+    }else if (key == 89) {
+    document.body.style.backgroundColor= "yellow";
+    }else if (key == 82) {
+    document.body.style.backgroundColor= "red";
+    }else if (key == 80) {
+    document.body.style.backgroundColor= "purple";
+}}
