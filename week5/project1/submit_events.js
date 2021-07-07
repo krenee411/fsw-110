daf0f3d0a47020fb8c367730ec08fddad07d4997
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-07-07 17:32:42 -0500

    submit_events added

---
+++ week5/project1/submit_events.js
@@ -0,0 +1,13 @@
+
+
+ function getdetails(){
+var a = document.getElementById("nameBox").value;
+var b = document.getElementById("dobBox").value;
+var c = document.getElementById("addBox").value;
+var d = document.getElementById("cityBox").value;
+var e = document.getElementById("stateBox").value;
+var f = document.getElementById("zipBox").value;
+
+
+alert("Name: "+a+ "  Date of Birth: "+b+ "  Address: "+c+ " " +d+ "," +e+ " " +f) 
+}
