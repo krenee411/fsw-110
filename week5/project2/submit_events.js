6ca56150509f7f02f0a84cfc8b40a95df7ff3594
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-07-08 19:45:35 -0500

    submit form added

---
+++ week5/project2/submit_events.js
@@ -0,0 +1,18 @@
+
+
+ function info(){
+    var a = document.getElementById("fnameBox").value;
+    var b = document.getElementById("lnameBox").value;
+    var c = document.getElementById("ageBox").value;
+    var d = document.getElementById("yes").value;
+    var e = document.getElementById("clothes").value;
+    var f = document.getElementById("fabric").value;
+
+
+
+    
+     alert("Name: "+a+b+ "\nAge:" +c+ "\nNew Customer: " +d+ "\nClothing choice:"+e+ "\nFabric Choice: "+f) ;
+ }
+
+ 
+ 
