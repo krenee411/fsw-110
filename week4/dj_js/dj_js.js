077af920b72d076ce36030feb6d839023883c8c4
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-07-17 12:43:05 -0500

    workbook added

--- week4/dj_js/dj_js.js
+++ week4/dj_js/dj_js.js
@@ -1,66 +1,60 @@
 var div = document.createElement("div")
 div.setAttribute("id", "box")
-document.body.style.width= "300px"
-document.body.style.height= "300px"
-document.body.style.border= "black 2px solid"
+div.style.width= "300px"
+div.style.height= "300px"
+div.style.border= "black 2px solid"
+div.style.backgroundColor= "white"
 document.body.appendChild(div)
-
-var div1 = document.createElement("div")
-for (var i=0; i<= 20; i++){
-    var div1 = document.createElement("h1")
-div1.textContent= "Hello World"
-document.body.appendChild(div1)
-document.body.style.textAlign= "right"
-}
-
 
 
 function one(){
-    div.body.style.backgroundColor= "black"
+    div.style.backgroundColor= "black"
 
 }
 
 function two(){
-    document.body.style.backgroundColor= "green"
+    div.style.backgroundColor= "green"
 
 }  
 
 function three(){
-    document.body.style.backgroundColor= "yellow"
+    div.style.backgroundColor= "yellow"
 }
 
 function four(){
-    document.body.style.backgroundColor= "red"
+    div.style.backgroundColor= "red"
 }
 
 function five(){
-    document.body.style.backgroundColor= "blue"
+    div.style.backgroundColor= "blue"
 }
 
 function six(){
-    document.body.style.backgroundColor= "purple"
+    div.style.backgroundColor= "purple"
 }
 
+const box= document.getElementById("box");
+
 window.addEventListener("load", one);
-window.addEventListener("mouseover", two);
-window.addEventListener("mousedown", three);
-window.addEventListener("dblclick", four);
-window.addEventListener("mouseup", five);
+div.addEventListener("mouseover", two);
+div.addEventListener("mousedown", three);
+div.addEventListener("dblclick", four);
+div.addEventListener("mouseup", five);
 window.addEventListener("scroll", six);
 
 window.onkeydown = function(e) {
 var key = e.keyCode ? e.keyCode : e.which;
 
    if (key == 87) {
-    document.body.style.backgroundColor= "white";
+    div.style.backgroundColor= "white";
    }else if (key == 66) {
-    document.body.style.backgroundColor= "blue";
+   div.style.backgroundColor= "blue";
    }else if (key == 71) {
-    document.body.style.backgroundColor= "green";
+    div.style.backgroundColor= "green";
     }else if (key == 89) {
-    document.body.style.backgroundColor= "yellow";
+    div.style.backgroundColor= "yellow";
     }else if (key == 82) {
-    document.body.style.backgroundColor= "red";
+   div.style.backgroundColor= "red";
     }else if (key == 80) {
-    document.body.style.backgroundColor= "purple";
+    div.style.backgroundColor= "purple";
 }}
